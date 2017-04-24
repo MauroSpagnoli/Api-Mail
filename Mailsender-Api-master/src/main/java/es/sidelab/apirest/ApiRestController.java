99b202fdf5b0d37fab02c35a9b6a3d3f84e3f668
MauroSpagnoli <maurospagnoliherrera@hotmail.com>
2017-04-24 19:18:27 +0200

    Inicial Commit

---
+++ Mailsender-Api-master/src/main/java/es/sidelab/apirest/ApiRestController.java
@@ -0,0 +1,67 @@
+package es.sidelab.apirest;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiRestController {
+
+	@RequestMapping(value = "/crearpdf", method = RequestMethod.POST)
+	public ResponseEntity<String> sendEmail(@RequestParam("email") String email,@RequestParam("subject") String subject, @RequestParam("body") String body){
+		 System.out.println(email);
+		 System.out.println(subject);
+		 System.out.println(body);
+		 	final String username = "gamingvalleyurjc@gmail.com";
+	        final String password = "Davidcalvo69";
+
+	        Properties props = new Properties();
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.smtp.auth", "true");
+	        props.put("mail.smtp.host", "smtp.gmail.com");
+	        props.put("mail.smtp.port", "587");
+	        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+	        
+	        Session session = Session.getInstance(props,
+	          new javax.mail.Authenticator() {
+	            protected PasswordAuthentication getPasswordAuthentication() {
+	                return new PasswordAuthentication(username, password);
+	            }
+	          });
+
+	        try {
+
+	            Message message = new MimeMessage(session);
+	            message.setFrom(new InternetAddress("gamingvalleyurjc@gmail.com"));
+	            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(email));
+	            message.setSubject(subject);
+	            message.setText(body);
+
+	            Transport.send(message);
+
+	            System.out.println("Done");
+
+	        } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	        }
+
+		return new ResponseEntity<>("OK",HttpStatus.ACCEPTED);
+	}
+
+
+}
---
+++ Mailsender-Api-master/src/main/java/es/sidelab/apirest/Email.java
@@ -0,0 +1,15 @@
+package es.sidelab.apirest;
+public class Email {
+	private String email;
+	public Email(){
+		
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+}
